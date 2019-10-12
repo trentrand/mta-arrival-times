95830c3aa49331bf6d2d0c1fb93fe776b6dca509
Trent Rand <contact@trentrand.com>
2019-10-11 23:42:22 -0400

    Add xcode-generated Apple Watch app scaffolding

---
+++ MTA Arrival Times WatchKit Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MTA Arrival Times WatchKit Extension
+//
+//  Created by Trent Rand on 10/11/19.
+//  Copyright © 2019 Trent Rand. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ MTA Arrival Times WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  MTA Arrival Times WatchKit Extension
+//
+//  Created by Trent Rand on 10/11/19.
+//  Copyright © 2019 Trent Rand. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
